3ccff6d98a6b33203afb86e88072accee80150f6
Sean Christian Lindholm <swanglindholm@gmail.com>
2016-06-16 10:21:03 +0200

    SurfaceView + canvas

---
+++ app/src/main/java/com/mysketch/SketchDrawing.java
@@ -0,0 +1,20 @@
+package com.mysketch;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+
+/**
+ * Created by Sean on 16-06-2016.
+ */
+public class SketchDrawing extends Activity{
+    private static final String TAG = "Making of a SurfaceView";
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
