47e0b563dfbbe6d33cb8bdd3dd95f4efec1f338b
Benjamin <benja.e95@gmail.com>
2016-06-16 10:34:54 +0200

    Loading screen with wrong logo

---
+++ app/src/main/java/com/mysketch/Loading.java
@@ -0,0 +1,54 @@
+package com.mysketch;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import com.example.benjamin.git.MySketch.R;
+
+
+/**
+ * Created by Benjamin on 15-06-2016.
+ */
+public class Loading extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.loading);
+        final ImageView imageView = (ImageView) findViewById(R.id.imageView);
+
+        Resources res = getBaseContext().getResources();
+        int id = R.drawable.logo3;
+        Bitmap b = BitmapFactory.decodeResource(res, id);
+
+        final Animation animation = AnimationUtils.loadAnimation(getBaseContext(), R.anim.rotate);
+        final Animation endAnimation = AnimationUtils.loadAnimation(getBaseContext(), R.anim.abc_fade_out);
+
+        imageView.startAnimation(animation);
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                imageView.startAnimation(endAnimation);
+                finish();
+                Intent mainActivity = new Intent(getBaseContext(), MainActivity.class);
+                startActivity(mainActivity);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/mysketch/MainActivity.java
@@ -0,0 +1,18 @@
+package com.mysketch;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.benjamin.git.MySketch.R;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
