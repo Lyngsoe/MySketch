73ffce54ff3c63da72925cd49e8760008a291404
Benjamin <benja.e95@gmail.com>
2016-06-16 11:12:49 +0200

    btn names and setup begun

--- app/src/main/java/com/mysketch/MainActivity.java
+++ app/src/main/java/com/mysketch/MainActivity.java
@@ -1,7 +1,10 @@
 package com.mysketch;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.benjamin.git.MySketch.R;
 
@@ -14,5 +17,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    final Button newProject = (Button) findViewById(R.id.btn_new);
+        newProject.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), SetupProject.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/mysketch/SetupProject.java
@@ -0,0 +1,7 @@
+package com.mysketch;
+
+/**
+ * Created by Benjamin on 16-06-2016.
+ */
+public class SetupProject {
+}
