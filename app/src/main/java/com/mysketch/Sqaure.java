fd53fa7418ffacf2c801d5f610d9a8190a6524cc
Lyngsoe <s134280@student.dtu.dk>
2016-06-17 10:16:39 +0200

    jonas push

--- app/src/main/java/com/mysketch/Sqaure.java
+++ app/src/main/java/com/mysketch/Sqaure.java
@@ -5,38 +5,48 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.PointF;
+import android.graphics.RectF;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
 import com.example.benjamin.git.MySketch.R;
 
 public class Sqaure extends View {
-    private static final int BITMAP_SIZE = 64;
-    private static final int REFRESH_RATE = 40;
+
+    final String TAG = "Square";
     private final Paint mPainter = new Paint();
-    private Bitmap mBitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.sq);
     float x;
     float y;
+    float drawX,drawY;
+
 
     public Sqaure(Context context, float x, float y){
         super(context);
         this.x = x;
         this.y = y;
-        mPainter.setAntiAlias(true);
-        invalidate();
+
     }
     @Override
     protected synchronized void onDraw(Canvas canvas) {
 
         // TODO - save the canvas
         canvas.save();
-
-        // TODO - draw the bitmap at its new location
-        canvas.drawBitmap(mBitmap, x, y,mPainter);
+        // TODO - draw at its new location
+        canvas.drawCircle(x-drawX,y-drawY,100,mPainter);
+        //canvas.drawCircle(x-getTranslationX(),y-getTranslationY(),100,mPainter);
 
         // TODO - restore the canvas
         canvas.restore();
     }
 
+    public void setDrawX(float drawX) {
+        this.drawX = drawX;
+    }
+
+    public void setDrawY(float drawY) {
+        this.drawY = drawY;
+    }
 }
