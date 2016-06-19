8bfc389af64a96d9b8aba83aed3d8494d508f25d
SlapTurtle <michaeldkirkegaard@gmail.com>
2016-06-19 14:48:40 +0200

    Michael Commit : Ret stor ændring, men nødvendig

--- app/src/main/java/com/mysketch/Shapes.java
+++ app/src/main/java/com/mysketch/Shapes.java
@@ -2,33 +2,72 @@ package com.mysketch;
 
 import android.content.Context;
 import android.graphics.PointF;
-import android.view.GestureDetector;
 import android.view.View;
 
 /**
  * Created by Sean on 17-06-2016.
  */
-public class Shapes extends View {
+public abstract class Shapes extends View {
     float mScaleFactor = 1;
     PointF zoomPoint;
 
-    public Shapes(Context context){
+    float x;
+    float y;
+    float drawX;
+    float drawY;
+
+    String projectName;
+    int uniqueID;
+    String shapeType;
+
+    public Shapes(Context context, String projectName, String shapeType, boolean addInstance){
         super(context);
+        this.projectName = projectName;
+        this.shapeType = shapeType;
+        if(addInstance) {
+            this.uniqueID = DataManager.getUniqueID(projectName);
+        }
     }
 
-    public boolean Intersects(float x, float y){return false;}
+    public void setDraw(float x, float y){
+        this.drawX=x;
+        this.drawY=y;
+    }
 
-    public void setDraw(float x, float y){}
+    public void setCoord(float x, float y){
+        this.x = x;
+        this.y = y;
+    }
 
-    public float getXCoord(){return -1.0f;}
+    public float getX(){
+        return x;
+    }
 
-    public float getYCoord(){return -1.0f;}
+    public float getY(){
+        return y;
+    }
 
-    public void Move(float dx, float dy){}
+    public float getXCoord(){
+        return drawX;
+    }
 
-    public void setScale(float scale){mScaleFactor = scale;}
+    public float getYCoord(){
+        return drawY;
+    }
 
-    public void setZoomPoint(PointF zoomPoint){this.zoomPoint = zoomPoint;}
+    public void setScale(float scale){
+        mScaleFactor = scale;
+    }
+
+    public void setZoomPoint(PointF zoomPoint){
+        this.zoomPoint = zoomPoint;
+    }
+
+    public abstract boolean Intersects(float x, float y);
+
+    public abstract void Move(float dx, float dy);
+
+
 
 
 
--- app/src/main/java/com/mysketch/Square.java
+++ app/src/main/java/com/mysketch/Square.java
@@ -4,70 +4,52 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.drawable.Drawable;
-import android.view.GestureDetector;
-import android.view.SurfaceView;
-import android.view.View;
 
 /**
  * Created by Sean on 16-06-2016.
  */
 public class Square extends Shapes {
 
+    final static String SHAPE_TYPE = "SQUARE";
+
+    final static String LOGTAG = "Circle";
 
     private Paint paint = new Paint();
-    private float h,w;
-    private float x,y = 100;
-    private float drawX;
-    private float drawY;
+    float h,w;
 
-    public  Square(Context context, float w, float h, float x, float y){
-        super(context);
+    public  Square(Context context, String projectName, boolean addInstance, float w, float h, float x, float y){
+        super(context, projectName, SHAPE_TYPE, addInstance);
         paint.setColor(Color.BLACK);
         paint.setStyle(Paint.Style.STROKE);
         paint.setAntiAlias(true);
         paint.setStrokeWidth(4.5f);
         this.h = h;
         this.w = w;
+        this.x = x;
+        this.y = y;
+
+        if(addInstance){
+            DataManager.saveAndOverwriteSingleShape(this);
+        }
 
         invalidate();
-
-
     }
 
     public boolean Intersects(float x, float y){
-
-       // if()
-
-        return true;
+        //TODO
+        return false;
     }
 
-    public void setCoord(float x, float y){
-        this.x = x;
-        this.y = y;
+    @Override
+    public void Move(float dx, float dy) {
+        //TODO
     }
-
-    public void setDraw(float x, float y){
-        this.drawX=x;
-        this.drawY=y;
-    }
-
-    public float getXCoord(){
-        return drawX;
-    }
-
-    public float getYCoord(){
-        return drawY;
-    }
-
-
 
     @Override
     public void onDraw(Canvas canvas){
 
-
-
         canvas.save();
+
         canvas.scale(mScaleFactor, mScaleFactor,zoomPoint.x,zoomPoint.y);
         canvas.drawLine(x-drawX,y-drawY,x-drawX+w,y-drawY,paint);
         canvas.drawLine(x-drawX,y-drawY,x-drawX,y-drawY+h,paint);
